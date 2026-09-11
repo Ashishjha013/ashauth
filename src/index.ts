62b9630b0a97eeca1a3b8ece717d3f14922b398d
Ashish Jha <ashishjha1304@gmail.com>
2026-09-11 12:33:31 +0530

    fix: ensure server listens on correct port and handle default value

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,10 @@ import createApplication from "./app/app.js";
 
 const startServer = async () => {
   const server = createServer(createApplication());
-  const PORT = process?.env?.PORT;
-  server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  const PORT = Number(process.env.PORT) || 8080;
+
+  server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
   });
 };
 
