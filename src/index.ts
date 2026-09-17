c2069ad3eae2b92e26ad4bb313bc014698c0509f
Ashish Jha <ashishjha1304@gmail.com>
2026-09-18 04:46:48 +0530

    feat: add favicon to multiple HTML pages and update server log message

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const startServer = async () => {
   const PORT = Number(process.env.PORT) || 8080;
 
   server.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
   });
 };
 
